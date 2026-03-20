2a8ff42334917300e7f35b8966dfafce3a896246
Julio Saboia <julio.saboia.1@gmail.com>
2026-03-20 15:46:57 -0300

    fix package.json

---
+++ Frontend/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div style="text-align:center; margin-top:50px;">
+    <h1>Sistema Clínica</h1>
+    <button @click="testar">Testar API</button>
+    <p>{{ mensagem }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      mensagem: ''
+    }
+  },
+  methods: {
+async testar() {
+  try {
+    const res = await axios.get('https://clinica-app-vutp.onrender.com');
+    this.mensagem = res.data;
+  } catch (err) {
+    alert("erro na API");
+    console.log(err);
+  }
+}
+  }
+}
+</script>
